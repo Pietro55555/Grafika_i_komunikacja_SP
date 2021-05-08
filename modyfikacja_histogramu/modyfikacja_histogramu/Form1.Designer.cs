3aa0298f46ce325e51439e0a8cc76b032e727a06
Pietro55555 <80325475+Pietro55555@users.noreply.github.com>
2021-05-08 19:52:30 +0200

    Add files via upload

---
+++ modyfikacja_histogramu/modyfikacja_histogramu/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace modyfikacja_histogramu
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(166, 598);
+            this.panel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 171);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 27);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Gauss";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(90, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "r:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(108, 154);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            255,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(58, 23);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 138);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 27);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Uśredniający";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(39, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Filtry";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 56);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Wyrównanie";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(39, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wczytaj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(166, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(482, 290);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(116, 122);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // panel3
+            // 
+            this.panel3.AutoScroll = true;
+            this.panel3.BackColor = System.Drawing.SystemColors.Control;
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel3.Location = new System.Drawing.Point(166, 320);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(482, 278);
+            this.panel3.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(72, 67);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel4.Location = new System.Drawing.Point(166, 290);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(482, 30);
+            this.panel4.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 598);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ modyfikacja_histogramu/modyfikacja_histogramu/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace modyfikacja_histogramu
+{
+    public partial class Form1 : Form
+    {
+        private int szer = 0, wys = 0;
+        private int[] red = null;
+        private int[] green = null;
+        private int[] blue = null;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] tonred = tablica_tonalna(red, szer * wys);
+            int[] tongreen = tablica_tonalna(green, szer * wys);
+            int[] tonblue = tablica_tonalna(blue, szer * wys);
+            Color k,j;
+           Bitmap photo1=(Bitmap)pictureBox1.Image;
+           Bitmap photo2 = (Bitmap)pictureBox2.Image;
+
+            for (int x = 0; x < szer; x++)
+            {
+                for (int y = 0; y < wys; y++)
+                {
+                    j = photo1.GetPixel(x, y);
+                    k = Color.FromArgb(tonred[j.R], tongreen[j.G], tonblue[j.B]);
+                    photo2.SetPixel(x, y, k);     
+                }
+            }
+            pictureBox2.Invalidate();
+
+        }
+
+        private int[] tablica_tonalna(int[] values, int size)
+        {
+            //poszukaj wartości minimalnej
+            double minValue = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                if (values[i] != 0)
+                {
+                    minValue = values[i];
+                    break;
+                }
+            }
+
+            int[] result = new int[256];
+            double sum = 0;
+            for (int i = 0; i < 256; i++)
+            {
+                sum += values[i];
+                result[i] = (int)(((sum - minValue) / (size - minValue)) * 255.0);
+            }
+
+            return result;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap photo1 = (Bitmap)pictureBox1.Image;
+            Bitmap photo2 = (Bitmap)pictureBox2.Image;
+            int prom = (int)numericUpDown1.Value;
+            int[,] maska = new int[3, 3];
+
+            maska[0, 0] = prom;
+            maska[0, 1] = prom;
+            maska[0, 2] = prom;
+
+            maska[1, 0] = prom;
+            maska[1, 1] = prom;
+            maska[1, 2] = prom;
+
+            maska[2, 0] = prom;
+            maska[2, 1] = prom;
+            maska[2, 2] = prom;
+
+            int norm = 0;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    norm += maska[i, j];        //normowanie maski
+
+            int r, g, b;
+            Color k;
+            for (int x = 1; x < szer - 1; x++)
+            {
+                for (int y = 1; y < wys - 1; y++)
+                {
+                    r = g = b = 0;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            k = photo1.GetPixel(x + i - 1, y + j - 1);
+                            r += k.R * maska[i, j];
+                            g += k.G * maska[i, j];
+                            b += k.B * maska[i, j];
+                        }
+                    }
+                    if (norm != 0)
+                    {
+                        r /= norm;
+                        g /= norm;
+                        b /= norm;
+                    }
+
+                    if (r < 0)
+                        r = 0;
+
+                    if (r > 255)
+                        r = 255;
+
+                    if (g < 0)
+                        g = 0;
+
+                    if (g > 255)
+                        g = 255;
+
+                    if (b < 0)
+                        b = 0;
+
+                    if (b > 255)
+                        b = 255;
+
+                    photo2.SetPixel(x, y, Color.FromArgb(r, g, b));
+
+                }
+            }
+            pictureBox2.Invalidate();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Bitmap photo1 = (Bitmap)pictureBox1.Image;
+            Bitmap photo2 = (Bitmap)pictureBox2.Image;
+            int prom = (int)numericUpDown1.Value;
+            int[,] maska = new int[3, 3];
+
+            maska[0, 0] = maska[1, 0] / 2;
+            maska[0, 1] = prom/2;
+            maska[0, 2] = maska[1, 2]/2;
+
+            maska[1, 0] = prom / 2;
+            maska[1, 1] = prom;
+            maska[1, 2] = prom / 2;
+
+            maska[2, 0] = maska[1, 0] / 2;
+            maska[2, 1] = prom/2;
+            maska[2, 2] = maska[1, 2] / 2;
+
+            int norm = 0;
+
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    norm += maska[i, j];        //normowanie maski
+
+            int r, g, b;
+            Color k;
+            for (int x = 1; x < szer - 1; x++)
+            {
+                for (int y = 1; y < wys - 1; y++)
+                {
+                    r = g = b = 0;
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            k = photo1.GetPixel(x + i - 1, y + j - 1);
+                            r += k.R * maska[i, j];
+                            g += k.G * maska[i, j];
+                            b += k.B * maska[i, j];
+                        }
+                    }
+                    if (norm != 0)
+                    {
+                        r /= norm;
+                        g /= norm;
+                        b /= norm;
+                    }
+
+                    if (r < 0)
+                        r = 0;
+
+                    if (r > 255)
+                        r = 255;
+
+                    if (g < 0)
+                        g = 0;
+
+                    if (g > 255)
+                        g = 255;
+
+                    if (b < 0)
+                        b = 0;
+
+                    if (b > 255)
+                        b = 255;
+
+                    photo2.SetPixel(x, y, Color.FromArgb(r, g, b));
+
+                }
+            }
+            pictureBox2.Invalidate();
+        }
+
+
+        //wczytaj///////////////////////////////////////////////////////////////////
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+                szer = pictureBox1.Image.Width;
+                wys = pictureBox1.Image.Height;
+                pictureBox2.Image = new Bitmap(szer, wys);
+
+                //Obliczam histogram
+                red = new int[256];
+                green = new int[256];
+                blue = new int[256];
+                for (int x = 0; x < szer; x++)
+                {
+                    for (int y = 0; y < wys; y++)
+                    {
+                        Color pixel = ((Bitmap)pictureBox1.Image).GetPixel(x, y);
+                        red[pixel.R]++;
+                        green[pixel.G]++;
+                        blue[pixel.B]++;
+                    }
+                }
+            }
+        }
+    }
+}
